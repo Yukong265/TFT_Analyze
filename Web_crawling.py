12ba8587678f0d0b28ff33bed5c79ba3bec22c8d
Yu Bin <subenmoom50@gmail.com>
2022-06-23 19:22:41 +0900

    🎈:: Bring deck name in data

--- Web_crawling.py
+++ Web_crawling.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = r'http://127.0.0.1:5500/web.html'
 req = requests.get(url)
@@ -12,3 +13,12 @@ if req.status_code == requests.codes.ok:
 
 else:
     print('failed')
+
+for i in range(2,33):
+    if i == 7:
+        continue
+    rst = str(soup.select(f'#decks > div > section:nth-child(4) > div.col-12.col-xl-9 > div:nth-child({i}) > header > h4 > a > b'))
+    r = re.sub('<.+?>',"", rst)
+    deck = r.lstrip('[').rstrip(']')
+
+    decks.append(deck)
